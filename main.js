b73e44d94d2896ad155f8036614ab8af6ae832b3
Vickouma77 <Vickprestone20@gmail.com>
2023-11-29 12:13:29 +0300

    update

---
+++ main.js
@@ -0,0 +1,16 @@
+const btns = document.querySelectorAll(".faq-toggle");
+
+const faq = document.querySelectorAll(".faq");
+/*
+for (let i = 0; i < btns.length; i++) {
+     btns[i].addEventListener("click", () => {
+     console.log("click");
+     faq[i].classList.toggle("active");
+   });
+} */
+
+btns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.parentNode.classList.toggle("active");
+  })
+})
