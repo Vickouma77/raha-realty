b40417342a54887e5bb2fc84453e7f192860d3dc
Vickouma77 <Vickprestone20@gmail.com>
2025-01-27 22:08:35 +0300

    feat(property-service): add Actix Web server to property-service
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- sentinel/property-service/src/main.rs
+++ sentinel/property-service/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn greet() -> impl Responder {
+    "Micro-services with rust and actix-web"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
