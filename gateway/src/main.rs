0ea4c6fad007afbe136390f2ac9997b6914c86d5
Vickouma77 <Vickprestone20@gmail.com>
2025-06-21 21:28:42 +0300

    feat(gateway): update main function to include logger initialization and return Result type

--- gateway/src/main.rs
+++ gateway/src/main.rs
@@ -1,4 +1,19 @@
+use actix_web::{middleware, App, HttpServer};
+use log::info;
+
 #[actix_web::main]
-async fn main() {
-    
+async fn main() -> std::io::Result<()> {
+
+    // Initialize the logger
+    env_logger::init();
+
+    info!("Starting the gateway server on port 8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::Logger::default())
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
